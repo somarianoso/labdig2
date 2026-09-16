b5114a8f9c59568ff6c5e27111cfd0f6725320da
Sophia <sophiasoaresmar@gmail.com>
2026-09-16 10:51:27 -0300

    Circuito desafio

--- experiencia3/exp3-arquivos/exp3_desafio.v
+++ experiencia3/exp3-arquivos/exp3_desafio.v
@@ -17,3 +17,67 @@ module exp3_desafio (
  output wire [1:0] db_posicao, // depuração do sinal posicao (servo)
  output wire [6:0] db_estado // depuracao: estado da UC
 );
+
+
+    // Sinais internos
+    wire        s_medir  ;
+    wire        s_trigger;
+    wire [11:0] s_medida ;
+    wire [3:0]  s_estado ;
+
+    // Circuito de interface com sensor
+    interface_hcsr04 INT (
+        .clock    (clock    ),
+        .reset    (reset    ),
+        .medir    (s_medir  ),
+        .echo     (echo     ),
+        .trigger  (s_trigger),
+        .medida   (s_medida ),
+        .pronto   (pronto   ),
+        .db_estado(s_estado )
+    );
+
+    // Displays para medida (4 dígitos BCD)
+    hexa7seg H0 (
+        .hexa   (s_medida[3:0]), 
+        .display(hex0         )
+    );
+    hexa7seg H1 (
+        .hexa   (s_medida[7:4]), 
+        .display(hex1         )
+    );
+    hexa7seg H2 (
+        .hexa   (s_medida[11:8]), 
+        .display(hex2          )
+    );
+
+    // Trata entrada medir (considerando borda de subida)
+    edge_detector DB (
+        .clock(clock  ),
+        .reset(reset  ),
+        .sinal(medir  ), 
+        .pulso(s_medir)
+    );
+
+    // Sinais de saída
+    assign trigger = s_trigger;
+
+    // Sinal de depuração (estado da UC)
+    hexa7seg H5 (
+        .hexa   (s_estado ), 
+        .display(db_estado)
+    );
+    assign db_echo    = echo;
+    assign db_trigger = s_trigger;
+    assign db_medir   = medir;
+
+    controle_servo servo (
+        .clock(clock),
+        .reset(reset)
+        .posicao(posicao),
+        .controle(controle_servo),
+        .db_controle(db_controle)
+    )
+
+
+endmodule
