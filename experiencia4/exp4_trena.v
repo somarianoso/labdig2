9e53fac3be5ac4185a9061831f87beccad4f707f
gabrielprodossimo <204345015+gabrielprodossimo@users.noreply.github.com>
2026-09-17 14:19:23 -0300

    exp4 trena

---
+++ experiencia4/exp4_trena.v
@@ -0,0 +1,19 @@
+module exp4_trena ( 
+input clock, 
+input reset,         
+input mensurar,          
+input echo,         
+output trigger,      
+output saida_serial,        
+output  [6:0] medida0, 
+output  [6:0] medida1, 
+output  [6:0] medida2, 
+output pronto, 
+output db_mensurar, 
+output db_echo,      
+output db_trigger,       
+output db_saida_serial,
+output  [6:0] db_estado 
+);
+
+endmodule
